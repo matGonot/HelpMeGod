02bb44ecfd39a78a875be6308be3c591b13bd56a
Mathias Salle 3eme année <e>
2020-03-12 18:06:29 +0100

    Que dieu me garde

--- Assets/Scripts/HP.cs
+++ Assets/Scripts/HP.cs
@@ -6,13 +6,25 @@ public class HP : MonoBehaviour
 {
     public float healthPoint;
     public GameObject smoke;
+    public GameObject spark;
+    public GameObject spark2;
+    float initHp;
     private void Start()
     {
+        initHp = healthPoint;
         smoke.SetActive(false);
+        spark.SetActive(false);
+        spark2.SetActive(false);
     }
     public void getDamaged(float dmg)
     {
         healthPoint -= dmg;
+        spark.SetActive(true);
+        if (healthPoint <= initHp / 2)
+        {
+            spark2.SetActive(true);
+            Debug.Log(gameObject.name + " 2 step");
+        }
         if(healthPoint <= 0)
         {
             smoke.SetActive(true);
--- Assets/Scripts/Misiles.cs
+++ Assets/Scripts/Misiles.cs
@@ -5,31 +5,39 @@ using UnityEngine;
 public class Misiles : MonoBehaviour
 {
     public float height;
-    private Vector3 origin;
     public GameObject player;
     public float speed;
+    public float offsetSign;
+    private Vector3 origin;
     bool isPointed = false;
     Vector3 targetPos;
+    public GameObject pointLight;
+    public float explosionRadius;
+    public LayerMask playerLayer;
     private void Start()
     {
         origin = transform.position;
+        pointLight.SetActive(false);
     }
     void Launch()
     {
         transform.Translate(new Vector3(origin.x, origin.y + height, origin.z) * speed * Time.deltaTime);
+        targetPos = player.transform.position;
+        pointLight.SetActive(true);
+        pointLight.transform.position = new Vector3(targetPos.x, targetPos.y + offsetSign, targetPos.z);
     }
     void Point()
     {
         Vector3 relativePos = player.transform.position - transform.position;
         Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.down);
         transform.rotation = rotation;
-        targetPos = player.transform.position;
         isPointed = true;
+
     }
     void Fire()
     {
         Debug.Log(targetPos);
-        transform.Translate(targetPos * Time.deltaTime * 2);
+        transform.Translate(targetPos * Time.deltaTime * speed);
     }
     private void Update()
     {
@@ -49,6 +57,17 @@ public class Misiles : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawSphere(new Vector3(origin.x, origin.y + height, origin.z), 2);
+        Gizmos.DrawSphere(transform.position, explosionRadius);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Ground")
+        {
+            RaycastHit hit;
+            Physics.SphereCast(transform.position, explosionRadius, Vector3.zero, out hit, playerLayer.value);
+            Debug.Log(hit.transform.name);
+            Destroy(pointLight);
+            Destroy(gameObject);
+        }
     }
 }
